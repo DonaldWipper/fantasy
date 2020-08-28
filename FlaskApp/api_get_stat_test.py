a2fff3a912fabd705c45481a8e088820247da0bf
d.utiralov <d.utiralov@talenttech.ru>
2020-08-28 11:11:38 +0300

    add france championship

---
+++ FlaskApp/api_get_stat_test.py
@@ -0,0 +1,23 @@
+from sport_api import SportsApiMethods
+import json
+
+def read_params(fn):
+    d = {}
+    try:
+        with open(fn, 'r', encoding="utf-8") as file:
+            d = json.load(file)
+    except FileNotFoundError:
+        print("Error. Can't find file " + fn)
+        d = {}
+    return d
+
+try:
+    settings = read_params("FlaskApp/settings.json")
+    sports = SportsApiMethods(settings)
+except:
+    settings = read_params("settings.json")
+    sports = SportsApiMethods(settings)
+
+
+
+print(sports.getStatSeasonsById(51))
